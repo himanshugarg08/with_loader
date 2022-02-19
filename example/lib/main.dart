0726239f71b17fab5a7b789e1e4dd1cc222af9b4
himanshugarg08 <himanshu.dma@gmail.com>
2022-02-19 21:55:06 +0530

    updated readme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:example/src/with_loader_example.dart';
+import 'package:example/backend_service.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:with_loader/with_loader.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +17,47 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: const Scaffold(body: WithLoaderExample()),
+      home: Scaffold(
+          body: Center(
+        child: WithLoader<String>(
+          future: apiCall,
+          successCondition: (result) => result == "Success",
+          loaderAlignment: Alignment.center,
+          onSuccess: () {
+            if (kDebugMode) {
+              print("Api Success");
+            }
+          },
+          onFailure: () {
+            if (kDebugMode) {
+              print("Api Failure");
+            }
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.purple.shade400,
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 32.0,
+                vertical: 16.0,
+              ),
+              child: Text(
+                "Button",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+          loader: CircularProgressIndicator(
+            color: Colors.purple.shade600,
+          ),
+        ),
+      )),
     );
   }
 }
